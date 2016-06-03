5c047f06c18ed309cc30d219400e8d198ceafd6a
Changyeon Lee <lisyoen@gmail.com>
2016-06-03 21:08:59 +0900

    initialize

---
+++ inc/teammanageredc.h
@@ -0,0 +1,23 @@
+#ifndef __teammanageredc_H__
+#define __teammanageredc_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "teammanageredc"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.teammanageredc"
+#endif
+
+#define EDJ_FILE "edje/teammanageredc.edj"
+#define GRP_MAIN "main"
+
+
+#endif /* __teammanageredc_H__ */
